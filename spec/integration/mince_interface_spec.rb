4d04e8726007974e3f1cc5981f579ea6ba2213a7
Matt Simpson <matt.simpson3@gmail.com>
2012-10-02 14:34:23 -0400

    Clear data store when integration tests finish so that we can run ints and units together

--- spec/integration/mince_interface_spec.rb
+++ spec/integration/mince_interface_spec.rb
@@ -6,5 +6,9 @@ describe 'Mince Interface with HashyDb' do
     Mince::Config.interface = Mince::HashyDb::Interface
   end
 
+  after do
+    Mince::HashyDb::Interface.clear
+  end
+
   it_behaves_like 'a mince interface'
 end
